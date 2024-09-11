209167038384fb7ee5b6f69dc2005d07f5658afc
hackCat1236 <88300979+hackCat1236@users.noreply.github.com>
2024-09-11 13:10:02 +0530

    Save Update Delete Customer Model

--- src/edu/ijse/mvc/Main.java
+++ src/edu/ijse/mvc/Main.java
@@ -19,6 +19,7 @@ public class Main {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         System.out.println("Hello");
         DBConnection.getInstance().getConnection();
+        System.out.println("Hello");
     }
     
 }
